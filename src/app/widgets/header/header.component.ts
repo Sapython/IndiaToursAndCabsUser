12c5cacf6cdad32024835b816bc1bac1193f6524
Saptam <saptampro2003@gmail.com>
2023-01-14 14:35:15 +0530

    Added sidebar

--- src/app/widgets/header/header.component.ts
+++ src/app/widgets/header/header.component.ts
@@ -8,8 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  links:Link[] = [
+    {
+      icon: "ri-pencil-line",
+      text: "Edit",
+      link: "/edit"
+    },
+    {
+      icon: "ri-pencil-line",
+      text: "Edit",
+      link: "/edit"
+    },
+    {
+      icon: "ri-pencil-line",
+      text: "Edit",
+      link: "/edit"
+    },
+    {
+      icon: "ri-pencil-line",
+      text: "Edit",
+      link: "/edit"
+    },
+    {
+      icon: "ri-pencil-line",
+      text: "Edit",
+      link: "/edit"
+    },
+  ]
+  userName= "Saptam"
+  userImage = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  sidebarOpen: boolean = false;
   ngOnInit(): void {
   }
 
+}
+
+type Link = {
+  icon:string,
+  text:string,
+  link:string
 }
